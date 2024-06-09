052ea042ef0d19e56526ca1ad0ef0dcf1ffad83d
no4mat <ryuuaka.mail@gmail.com>
2024-06-09 03:38:07 +0000

    Updates

--- packages.sh
+++ packages.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-apt install git xserver-xorg xinit bspwm compton nitrogen rofi polybar firefox-esr pavucontrol lxappearance dash pcmanfm lxterminal xarchiver alsa-utils l3afpad
+apt install git xserver-xorg xinit bspwm compton nitrogen rofi polybar firefox-esr pavucontrol lxappearance dash pcmanfm lxterminal xarchiver alsa-utils l3afpad feh

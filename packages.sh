b0c7a7dbaecccc697132bb48402f7837758f512a
= <=>
2024-07-01 06:36:57 +0000

    updates

--- packages.sh
+++ packages.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-apt install git xserver-xorg xinit bspwm compton nitrogen rofi polybar firefox-esr pavucontrol lxappearance dash pcmanfm lxterminal xarchiver alsa-utils l3afpad feh connman cmst brightnessctl libnotify-bin dunst lxpolkit
+apt install git xserver-xorg xinit bspwm compton nitrogen rofi polybar firefox-esr pavucontrol lxappearance dash pcmanfm lxterminal xarchiver alsa-utils l3afpad feh connman cmst brightnessctl libnotify-bin dunst lxpolkit nemo neofetch
